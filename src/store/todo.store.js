82e4f21d6b887d9c8ad9da37981ca92d50fec8e7
agustinzamudio <agusz147@hotmail.com>
2023-02-18 03:42:58 -0300

    Feat: add LocalStorage and filters todos

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -1,28 +1,32 @@
 import { Todo } from '../todos/models/todo.model'
 
-const Filters = {
+export const Filters = {
   All: 'all',
   Completed: 'completed',
   Pending: 'pending',
 }
 
 const state = {
-  todos: [
-    new Todo('Piedra del alma'),
-    new Todo('Piedra del infinito'),
-    new Todo('Piedra del tiempo'),
-    new Todo('Piedra del realidad'),
-  ],
+  todos: [],
   filter: Filters.All,
 }
 
 const initStore = () => {
-  console.log(state)
-  console.log('InitStore ToDo')
+  loadStore()
 }
 
 const loadStore = () => {
-  throw new Error('Not implemented')
+  if (!localStorage.getItem('state')) return
+
+  const { todos = [], filter = Filters.All } = JSON.parse(
+    localStorage.getItem('state')
+  )
+  state.todos = todos
+  state.filter = filter
+}
+
+const saveStateToLocalStorage = () => {
+  localStorage.setItem('state', JSON.stringify(state))
 }
 
 const getTodos = (filter = Filters.All) => {
@@ -45,6 +49,7 @@ const addTodo = description => {
   if (!description) throw new Error('Description is required')
 
   state.todos.push(new Todo(description))
+  saveStateToLocalStorage()
 }
 
 const toggleTodo = todoId => {
@@ -54,18 +59,22 @@ const toggleTodo = todoId => {
     }
     return todo
   })
+  saveStateToLocalStorage()
 }
 
 const deleteTodo = todoId => {
   state.todos = state.todos.filter(todo => todo.id !== todoId)
+  saveStateToLocalStorage()
 }
 
 const deleteCompleted = () => {
-  state.todos = state.todos.filter(todo => todo.done)
+  state.todos = state.todos.filter(todo => !todo.done)
+  saveStateToLocalStorage()
 }
 
 const setFilter = (newFilter = Filters.All) => {
   state.filter = newFilter
+  saveStateToLocalStorage()
 }
 
 const getCurrentFilter = () => {
--- src/todos/app.js
+++ src/todos/app.js
@@ -1,16 +1,24 @@
 import html from './app.html?raw'
-import todoStore from '../store/todo.store'
-import { renderTodos } from './use-cases'
+import todoStore, { Filters } from '../store/todo.store'
+import { renderTodos, renderPending } from './use-cases'
 
 const ElementIDs = {
+  ClearCompleted: '.clear-completed',
   TodoList: '.todo-list',
   NewTodoInput: '.new-todo',
+  TodoFilters: '.filter',
+  PendingCountLabel: '#pending-count',
 }
 
 export const App = elementId => {
   const displayTodos = () => {
     const todos = todoStore.getTodos(todoStore.getCurrentFilter())
     renderTodos(ElementIDs.TodoList, todos)
+    updatePendingCount()
+  }
+
+  const updatePendingCount = () => {
+    renderPending(ElementIDs.PendingCountLabel)
   }
 
   // Cuando la función App() se llama
@@ -24,6 +32,8 @@ export const App = elementId => {
   // Referencias HTML
   const newDescriptionInput = document.querySelector(ElementIDs.NewTodoInput)
   const todoListUL = document.querySelector(ElementIDs.TodoList)
+  const clearCompletedButton = document.querySelector(ElementIDs.ClearCompleted)
+  const filtersLIs = document.querySelectorAll(ElementIDs.TodoFilters)
 
   // Listeners
   newDescriptionInput.addEventListener('keyup', event => {
@@ -48,5 +58,29 @@ export const App = elementId => {
 
     todoStore.deleteTodo(element.getAttribute('data-id'))
     displayTodos()
+  })
+
+  clearCompletedButton.addEventListener('click', () => {
+    todoStore.deleteCompleted()
+    displayTodos()
+  })
+
+  filtersLIs.forEach(element => {
+    element.addEventListener('click', element => {
+      filtersLIs.forEach(el => el.classList.remove('selected'))
+      element.target.classList.add('selected')
+      switch (element.target.text) {
+        case 'Todos':
+          todoStore.setFilter(Filters.All)
+          break
+        case 'Pendientes':
+          todoStore.setFilter(Filters.Pending)
+          break
+        case 'Completados':
+          todoStore.setFilter(Filters.Completed)
+          break
+      }
+      displayTodos()
+    })
   })
 }
